4110de438394e7a1edc922d3d7296cbdaa7c1c88
Shubham Tingare <shubhamtingare0661@gmail.com>
2026-07-15 01:25:54 +0530

    Add basic configurations in config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,9 +3,21 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests",
 
-  reporter: "html",
+  reporter: [["html"], ["list"]],
+  timeout: 30000,
+  retries: 1,
+  workers: 2,
 
   use: {
+    baseURL: "https://www.saucedemo.com",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
     trace: "on-first-retry",
+    browserName: "chromium",
+    viewport: {
+      width: 1920,
+      height: 1080,
+    },
+    headless: false,
   },
 });
