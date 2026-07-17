2c4540c1fd0a642ac2e6f57913ef93e5ab7104fb
Shubham Tingare <shubhamtingare0661@gmail.com>
2026-07-17 14:20:49 +0530

    Create loginPage and add basic methods and constructor

---
+++ pages/LoginPage.ts
@@ -0,0 +1,19 @@
+import { Page } from "@playwright/test";
+import { BasePage } from "./BasePage";
+
+export class LoginPage extends BasePage {
+  constructor(page: Page) {
+    super(page);
+  }
+  private readonly usernameInput;
+  private readonly passwordInput;
+  private readonly loginButton;
+  private readonly forgotPasswordLink;
+
+  async enterUsername() {}
+  async enterPassword() {}
+  async clickLogin() {}
+  async clickForgotPassword() {}
+
+  login(username: string, password: string) {}
+}
