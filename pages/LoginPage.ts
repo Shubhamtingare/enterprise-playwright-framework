8d11ec68e39b9dbba22a66c8884f89852b42e1aa
Shubham Tingare <shubhamtingare0661@gmail.com>
2026-07-19 11:26:43 +0530

    Complete logic for LoginPage

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -5,15 +5,31 @@ export class LoginPage extends BasePage {
   constructor(page: Page) {
     super(page);
   }
-  private readonly usernameInput;
-  private readonly passwordInput;
-  private readonly loginButton;
-  private readonly forgotPasswordLink;
+  private readonly usernameInput = this.page.getByPlaceholder("Username");
+  private readonly passwordInput = this.page.getByPlaceholder("Password");
+  private readonly loginButton = this.page.getByRole("button", {
+    name: "Login",
+  });
+  private readonly forgotPasswordLink = this.page.locator(
+    ".orangehrm-login-forgot-header",
+  );
 
-  async enterUsername(username: string): Promise<void> {}
-  async enterPassword(passowrd: string): Promise<void> {}
-  async clickLogin() {}
-  async clickForgotPassword() {}
+  async enterUsername(username: string): Promise<void> {
+    await this.usernameInput.fill(username);
+  }
+  async enterPassword(password: string): Promise<void> {
+    await this.passwordInput.fill(password);
+  }
+  async clickLoginButton(): Promise<void> {
+    await this.loginButton.click();
+  }
+  async clickForgotPassword(): Promise<void> {
+    await this.forgotPasswordLink.click();
+  }
 
-  async login(username: string, password: string): Promise<void> {}
+  async login(username: string, password: string): Promise<void> {
+    await this.enterUsername(username);
+    await this.enterPassword(password);
+    await this.clickLoginButton();
+  }
 }
