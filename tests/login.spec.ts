46ce787a76b4b182d5b8a15c8cf824c336670b84
Shubham Tingare <shubhamtingare0661@gmail.com>
2026-07-26 16:59:28 +0530

    Grouping them into positive and negative scenarios

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -8,78 +8,82 @@ test.beforeEach("Navigate to correct URL", async ({ page }) => {
 });
 
 test.describe("OrangeHRM Login", () => {
-  test("Verify user can login with valid credentials", async ({ page }) => {
-    //Arrange
+  test.describe("Positive scenarios", () => {
+    test("Verify user can login with valid credentials", async ({ page }) => {
+      //Arrange
 
-    const username = LoginData.validUser.username;
-    const password = LoginData.validUser.password;
-    const loginPage = new LoginPage(page);
-    const dashboardpage = new DashboardPage(page);
+      const username = LoginData.validUser.username;
+      const password = LoginData.validUser.password;
+      const loginPage = new LoginPage(page);
+      const dashboardpage = new DashboardPage(page);
 
-    //Act
-    await loginPage.login(username, password);
+      //Act
+      await loginPage.login(username, password);
 
-    //Assert
-    await expect(dashboardpage.getDashboardHeading()).toContainText(
-      "Dashboard",
-    );
+      //Assert
+      await expect(dashboardpage.getDashboardHeading()).toContainText(
+        "Dashboard",
+      );
+    });
   });
 
-  test("Verify user cannot login with invalid username", async ({ page }) => {
-    //Arrange
-    const username = LoginData.invalidUser.username;
-    const password = LoginData.invalidUser.password;
-    const loginPage = new LoginPage(page);
+  test.describe("Negative scenarios", () => {
+    test("Verify user cannot login with invalid username", async ({ page }) => {
+      //Arrange
+      const username = LoginData.invalidUser.username;
+      const password = LoginData.invalidUser.password;
+      const loginPage = new LoginPage(page);
 
-    //Act
-    await loginPage.login(username, password);
+      //Act
+      await loginPage.login(username, password);
 
-    //Assert
-    await expect(loginPage.getLoginErrorMsg()).toContainText(
-      "Invalid credentials",
-    );
-  });
+      //Assert
+      await expect(loginPage.getLoginErrorMsg()).toContainText(
+        "Invalid credentials",
+      );
+    });
 
-  test("Verify user cannot login with invalid password", async ({ page }) => {
-    //Arrange
-    const username = LoginData.invalidPassword.username;
-    const password = LoginData.invalidPassword.password;
-    const loginPage = new LoginPage(page);
+    test("Verify user cannot login with invalid password", async ({ page }) => {
+      //Arrange
+      const username = LoginData.invalidPassword.username;
+      const password = LoginData.invalidPassword.password;
+      const loginPage = new LoginPage(page);
 
-    //Act
-    await loginPage.login(username, password);
+      //Act
+      await loginPage.login(username, password);
 
-    //Assert
-    await expect(loginPage.getLoginErrorMsg()).toContainText(
-      "Invalid credentials",
-    );
-  });
+      //Assert
+      await expect(loginPage.getLoginErrorMsg()).toContainText(
+        "Invalid credentials",
+      );
+    });
 
-  test("Verify user cannot login with empty username", async ({ page }) => {
-    //Arrange
-    const username = LoginData.emptyUsername.username;
-    const password = LoginData.emptyUsername.password;
-    const loginPage = new LoginPage(page);
+    test("Verify user cannot login with empty username", async ({ page }) => {
+      //Arrange
+      const username = LoginData.emptyUsername.username;
+      const password = LoginData.emptyUsername.password;
+      const loginPage = new LoginPage(page);
 
-    //Act
-    await loginPage.login(username, password);
+      //Act
+      await loginPage.login(username, password);
 
-    //Assert
-    await expect(loginPage.getUsernameReqErrMsg()).toContainText("Required");
-  });
+      //Assert
+      await expect(loginPage.getUsernameReqErrMsg()).toContainText("Required");
+    });
 
-  test.only("Verify user cannot login with empty password", async ({
-    page,
-  }) => {
-    //Arrange
-    const username = LoginData.emptyPassword.username;
-    const password = LoginData.emptyPassword.password;
-    const loginPage = new LoginPage(page);
+    test.only("Verify user cannot login with empty password", async ({
+      page,
+    }) => {
+      //Arrange
+      const username = LoginData.emptyPassword.username;
+      const password = LoginData.emptyPassword.password;
+      const loginPage = new LoginPage(page);
 
-    //Act
-    await loginPage.login(username, password);
+      //Act
+      await loginPage.login(username, password);
 
-    //Assert
-    await expect(loginPage.getPasswordReqErrMsg()).toContainText("Required");
+      //Assert
+      await expect(loginPage.getPasswordReqErrMsg()).toContainText("Required");
+    });
   });
 });
