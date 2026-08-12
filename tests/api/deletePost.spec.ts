6c45ae87b00f1810d47811bd93da4f82b60a3629
Shubham Tingare <shubhamtingare0661@gmail.com>
2026-08-12 19:41:45 +0530

    feat: add DELETE API tests and response validation

---
+++ tests/api/deletePost.spec.ts
@@ -0,0 +1,25 @@
+import test, { expect } from "@playwright/test";
+import { env } from "../../config/env";
+import { Logger } from "../../utils/Logger";
+
+test.describe("Delete Posts API", () => {
+  test("Verify DELETE removes a post", async ({ request }) => {
+    const response = await request.delete(`${env.apiUrl}/posts/1`);
+
+    const jsonData = await response.json();
+
+    Logger.info(JSON.stringify(jsonData, null, 2));
+    expect(response.status()).toBe(200);
+    expect(jsonData).toEqual({});
+  });
+
+  test("Verify DELETE removes a non-existing post", async ({ request }) => {
+    const response = await request.delete(`${env.apiUrl}/posts/999999`);
+
+    const jsonData = await response.json();
+    Logger.info(JSON.stringify(jsonData, null, 2));
+
+    expect(response.status()).toBe(200);
+    expect(jsonData).toEqual({});
+  });
+});
