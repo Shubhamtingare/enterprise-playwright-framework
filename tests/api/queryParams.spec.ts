7faf2e851f38f8008b2c47c43d7c3fdac6d050e8
Shubham Tingare <shubhamtingare0661@gmail.com>
2026-08-13 19:55:12 +0530

    feat: add API tests for path and query parameters

---
+++ tests/api/queryParams.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from "@playwright/test";
+import { env } from "../../config/env";
+import { Logger } from "../../utils/Logger";
+
+test.describe("Path and Query parameters API", () => {
+  test("Verify GET user using path parameter", async ({ request }) => {
+    const response = await request.get(`${env.apiUrl}/users/1`);
+
+    const jsonData = await response.json();
+
+    Logger.info(JSON.stringify(jsonData, null, 2));
+
+    expect(response.status()).toBe(200);
+    expect(jsonData).toHaveProperty("name");
+  });
+
+  test("Verify GET user using query parameter", async ({ request }) => {
+    const response = await request.get(`${env.apiUrl}/posts`, {
+      params: {
+        userId: 1,
+      },
+    });
+
+    const jsonData = await response.json();
+
+    Logger.info(JSON.stringify(jsonData, null, 2));
+
+    expect(response.status()).toBe(200);
+    expect(jsonData).toBeInstanceOf(Array);
+    expect(jsonData.length).toBeGreaterThan(0);
+
+    for (const post of jsonData) {
+      expect(post.userId).toBe(1);
+    }
+  });
+});
